96ae2501faa555a80c9f7216dc85e21f1581c452
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-01 01:33:09 -0400

    new file:   src/example2.cpp

---
+++ cpp-test/src/example2.cpp
@@ -0,0 +1,63 @@
+#include <network.hpp>
+#include <ucaspian.hpp>
+#include <utils/json_helpers.hpp>
+
+int main()
+{
+    const std::string kNetworkFilename = "networks/passnet.json";
+
+    const int kWidth = 5;
+    const int kHeight = 5;
+    const int kRuns = 10;
+
+    caspian::UsbCaspian proc;
+
+    auto net = caspian::Network(kWidth*kHeight);
+
+    // Read network from JSON file
+    std::ifstream fin;
+    fin.open(kNetworkFilename.c_str());
+    if (fin.fail()) {
+        std::cerr << "Couldn't open network file: " << kNetworkFilename;
+        exit(1);
+    }
+    nlohmann::json j;
+    fin >> j;
+    net.from_json(j);
+
+    // Configure the simulator with the new network
+    proc.configure(&net);
+
+    for (size_t i = 0; i < net.num_outputs(); i++) {
+        proc.track_timing(i);
+    }
+
+    const auto cycles = 3*kWidth + 2*kHeight;
+
+    for (int r=0; r<kRuns; ++r) {
+
+        // Queue up inputs
+        for (int i = 0; i < kHeight; ++i) {
+            proc.apply_input(i, 255, i);
+        }
+
+        // Simulate with sufficient time
+        proc.simulate(cycles);
+
+        printf("Simulate %4d\n", r);
+
+        for (int i=0; i<kHeight; ++i) {
+            printf("Output %d (%d):", i, proc.get_output_count(i));
+            //fmt::print("Output {} ({}):", i, proc.get_output_count(i));
+            auto outs = proc.get_output_values(i);
+            for (auto o : outs) {
+                printf(" %u", o);
+            }
+            printf("\n");
+        }
+
+        proc.clear_activity();
+    }
+
+    return 0;
+}
