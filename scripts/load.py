c1217c88539aa69576f00bd4edb575f3a7018954
Simon D. Levy <simon.d.levy@gmail.com>
2026-09-01 15:33:06 -0400

    modified:   example.py

---
+++ scripts/load.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import neuro
+import caspian
+
+NETWORK_FILE = "../networks/example.txt"
+
+net = neuro.Network()
+
+try:
+    net.read_from_file(NETWORK_FILE)
+except Exception:
+    print('Unable to read from ' + NETWORK_FILE)
+    exit(1)
+
+proc = caspian.Processor({"Backend": "uCaspian_USB"})
+
+proc.load_network(net)
